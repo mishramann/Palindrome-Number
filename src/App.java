0b5f7c76ee6b077c27ecf02258c515a8015f4797
mishramann <piyushmishra08oct@gmail.com>
2025-02-13 22:38:31 +0530

    Palindrome Number

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@
 import java.util.*;
 
 public class App {
-    public static void main(String[] args) {
+    public static void main (String[]args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter The Number");
         int n = sc.nextInt();
